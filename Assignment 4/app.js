1c827f48e7eef2fa3e522095712591701294d7c9
Megan Balcom <meg.balcom@gmail.com>
2016-02-07 16:08:11 -0500

    A4 4.4

--- Assignment 4/app.js
+++ Assignment 4/app.js
@@ -43,6 +43,20 @@
     };
   });
 
+   app.directive("productReviews", function(){
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+   app.directive("productSpecs", function(){
+    return{
+      restrict: 'A',
+      templateUrl: "product-specs.html"
+    };
+   });
+
   var gems = [{
     name: 'Azurite',
     description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
