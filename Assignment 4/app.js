a970f6f7678dc72ce889fec8606acb661aaef526
Megan Balcom <meg.balcom@gmail.com>
2016-02-07 17:33:40 -0500

    A4 4.7

--- Assignment 4/app.js
+++ Assignment 4/app.js
@@ -1,17 +1,35 @@
 (function() {
   var app = angular.module('gemStore', []);
 
+  app.directive('productGallery', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function(){
+        this.current = 0;
+        this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
   app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
+   app.controller('ReviewController', function(){
 
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
+    this.review = {};
+
+    this.addReview = function(product){
+      this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review = {};
     };
   });
+
 
   app.directive("productTabs", function(){
     return{
@@ -29,17 +47,6 @@
         };
       },
       controllerAs: "tab"
-    };
-  });
-
-  app.controller('ReviewController', function(){
-
-    this.review = {};
-
-    this.addReview = function(product){
-      this.review.createdOn = Date.now();
-      product.reviews.push(this.review);
-      this.review = {};
     };
   });
 
